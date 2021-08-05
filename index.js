22ce48b26901229f77e753fc7f4a3e499d011cf6
bemoodie <bmoodie30@gmail.com>
2021-08-04 22:52:23 -0400

    Update JS with Forecast API

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ function formatDate(todaysDate) {
   return `${day} | ${month} ${date},${year} | ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -68,6 +69,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "70ac119e9673d8efaba7774f355454f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   let iconElement = document.querySelector("#icon");
 
@@ -88,6 +97,7 @@ function showWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -164,4 +174,3 @@ let currentButton = document.querySelector("#current");
 currentButton.addEventListener("click", showCurrentLoaction);
 
 search("New york");
-displayForecast();
